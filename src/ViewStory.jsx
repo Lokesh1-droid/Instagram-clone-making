1c51113987bacb8070fa9affab80d01b1e467294
Lokesh1-droid <lokeshpartha0604@gmail.com>
2025-11-24 19:51:02 +0530

    feat:added useEffect in viewstory

--- src/ViewStory.jsx
+++ src/ViewStory.jsx
@@ -4,8 +4,10 @@ export default function ViewStory(){
   const {id} = useParams();
   const [story,setstory] = useState(null);
   useEffect(()=>{
-    
-  })
+    fetch('http://localhost:3000/ststories
+    .then((data)=>{data.json()})
+    .then((data)=>{setstory(data)})
+  },[])
   return(
     <div>ViewStory</div>
     )
